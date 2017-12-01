8baa25b573bf6d9ba1c474fb7e94e7d85a7d8876
dev-keenability <dev@keenability.com>
2017-12-01 11:56:10 -0500

    progress

--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
+// To run the app: node routes
+// Better yet is to run the app by typing the command: nodemon
+// nodemon will know where your entry file is and restart the server everytime you make changes to this file.
+// to install nodemon: npm install -g nodemon
+
 var express = require('express');
 var routes = express();
 var bodyParser = require('body-parser');
@@ -7,37 +12,34 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/node_api_v1')
 var db = mongoose.connection
 
+// https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html
+// mongoose was giving a blank array so used this instead
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/node_api_v1_development";
+
+var ObjectId = require('mongodb').ObjectId; // this is so that i can find object by its id
+
+
 Genre = require('./models/genre');
 Book = require('./models/book');
 
 
 
-// https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/node_api_v1_development";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  db.collection("customers").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
+routes.listen(3000);
+console.log('Running on port 3000...')
 
 
-// To run the app: node routes
-// Better yet is to run the app by typing the command: nodemon
-// nodemon will know where your entry file is and restart the server everytime you make changes to this file.
-// to install nodemon: npm install -g nodemon
 
 
 routes.get('/', function(req, res){
   res.send('Hello Worlds');
 });
 
+
+// INDEX
 routes.get('/api/v1/genres', function(req, res) {
-  // For some reason mongoose was not working so i decided to use mongo client instead
+  // For some reason mongoose was not working so i decided to use mongo client instead, hopefully there is a way to refactor this code
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     db.collection("genres").find().toArray(function(err, result) {
@@ -46,7 +48,6 @@ routes.get('/api/v1/genres', function(req, res) {
       db.close();
     });
   });
-
 });
 
 routes.get('/api/v1/books', function(req, res) {
@@ -58,6 +59,25 @@ routes.get('/api/v1/books', function(req, res) {
   });
 });
 
-routes.listen(3000);
-console.log('Running on port 3000...')
+
+// SHOW
+routes.get('/api/v1/genres/:_id', function(req, res) {
+  var id = req.params._id;
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    db.collection("genres").findOne({_id: ObjectId(id)}, function(err, result) {
+      if (err) throw err;
+      res.json(result);
+      db.close();
+    });
+  });
+});
+
+// POST
+
+// UPDATE
+
+// DELETE
+
+
 
